1b3355ac73be62b71f0aed61093199bedfb665fc
John Costa <john.costa@wexinc.com>
2020-05-24 15:02:51 -0400

    integration should fail

---
+++ datadogext/datadogext_test.go
@@ -0,0 +1,25 @@
+package datadogext_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/johncosta/datadog-github-action-poc/datadogext"
+)
+
+func TestDatadogEvent(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	event := datadogext.NewDatadogEvent()
+
+	assert.Equal(t, *event.GetSource(), datadogext.Github)
+	assert.NotEmpty(t, event.GetTime())
+	assert.Equal(t, *event.GetTitle(), "Test Event Title")
+	assert.Equal(t, event.GetTags(), []string{"app:TestApp", "env:Test"})
+	assert.Equal(t, *event.GetStatus(), "status:info")
+}
