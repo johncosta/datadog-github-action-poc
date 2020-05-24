de9263fb7ad6b13951d39bf4ea293461d38de147
John Costa <john.costa@wexinc.com>
2020-05-24 13:38:28 -0400

    debug

--- main.go
+++ main.go
@@ -2,13 +2,60 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
+
+	"github.com/zorkian/go-datadog-api"
 )
 
+const (
+	github string = "GITHUB"
+	low string = "LOW"
+)
 func main() {
-	myInput := os.Getenv("INPUT_DD_API_KEY")
+	ddApiKey := os.Getenv("INPUT_DD_API_KEY")
+	if ddApiKey == "" {
+		log.Fatalf("input DD_API_KEY must be set")
+	}
+	ddAppKey := os.Getenv("INPUT_DD_APP_KEY")
+	if ddAppKey == "" {
+		log.Fatalf("input DD_APP_KEY must be set")
+	}
+	client := datadog.NewClient(ddApiKey, ddAppKey)
 
-	output := fmt.Sprintf("Hello %s", myInput)
+	eventTitle := os.Getenv("INPUT_EVENT_TITLE")
+	if eventTitle == "" {
+		log.Fatalf("input EVENT_TITLE must be set")
+	}
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+	// Event Time in UTC
+	loc, _ := time.LoadLocation("UTC")
+	now := time.Now().In(loc)
+	fmt.Println("ZONE : ", loc, " Time : ", now) // UTC
+
+	unixNow := int(now.Unix())
+	source := github
+	event, err := client.PostEvent(&datadog.Event{
+		Id:          nil,  // Optional
+		Title:       &eventTitle,
+		Text:        nil,  // Optional
+		Time:        &unixNow,
+		Priority:    nil,  // Optional
+		AlertType:   nil,  // Optional
+		Host:        nil,  // Optional
+		Aggregation: nil,  // Optional
+		SourceType:  &source,
+		Tags:        nil,  // Optional
+		Url:         nil,  // Optional
+		Resource:    nil,  // Optional
+		EventType:   nil,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	output := fmt.Sprintf("Event: %+v", event)
+	fmt.Println(fmt.Sprintf(`::set-output name=output::%s`, output))
+
 }
